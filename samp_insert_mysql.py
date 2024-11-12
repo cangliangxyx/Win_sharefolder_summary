8d99c110f3b89d63c9748c04d2b47843deda5de1
vendor.liang.chang <changliang9897@gmail.com>
2024-11-12 15:07:07 +0800

    123123
    Signed-off-by: vendor.liang.chang <changliang9897@gmail.com>

--- samp_insert_mysql.py
+++ samp_insert_mysql.py
@@ -18,29 +18,32 @@ def insert_mysql(env,file_info, table_name):
                 row.append(date_str)
                 stdout_data = (row[0], row[1], row[2], row[3], row[4], row[5], row[6])
                 sql = 'INSERT INTO {} ({}) VALUES ({}{}{})'.format(table_name, ', '.join(columns), '"', '","'.join(stdout_data), '"')
-                print("update_sql =", sql)
+                # print("update_sql =", sql)
                 try:
                     dbconn.update_by_sql(sql)
                 except Error as e:
                     print("插入失败:", e)
-            print("插入数据完成")
+    print(table_name , "表完成插入数据" )
 
-#
+
 # from zip_file_info import zip_file_list
-# from traverse_folder import traverse_directory
+# from samp_traverse_folder import traverse_directory
+# from common.config import file_dir_config
 #
-# file_path = "C:\\Win_sharefolder_summary"
+# env = "dev"
+# file_path = file_dir_config(env)
 # print("工作目录 = ", file_path)
+#
 # # 获取zip文件列表
-# zip_files = zip_file_list("test")
+# zip_files = zip_file_list(env)
 # for i in zip_files:
 #     print(i)
-#     traverse_to_path = 'C:\\Win_sharefolder_summary\\' + os.path.splitext(i)[0]
+#     traverse_to_path = file_path + os.path.splitext(i)[0]
 #     print("traverse_to_path = ", traverse_to_path)
 #     # 遍历目录
 #     insert_file_list = traverse_directory(traverse_to_path)
 #     print("folder_info = ", insert_file_list)
-#     # 插入表名
+#     # 开始插入(表明忽略大小写)
 #     tabes_name = 'samp_' + os.path.splitext(i)[0][-4:]
 #     print("tabes_name = ", tabes_name)
-#     insert_mysql("test", insert_file_list, tabes_name)
+#     insert_mysql(env, insert_file_list, tabes_name.lower())
--- samp_main.py
+++ samp_main.py
@@ -4,24 +4,28 @@ from samp_clear_folder import clear_directories
 from samp_traverse_folder import traverse_directory
 from zip_file_info import zip_file_list
 from extract_files import extract_zip
+from common.config import file_dir_config
 
 def main():
     env = os.environ.get("python_variable", "default")
+    # env = "test"
     print("Please confirm the current environment. = ", env)
     input("Press Enter to continue...")
     # 解压
     extract_zip(env)
 
     # 获取zip文件列表
-    zip_files = zip_file_list("test")
+    print("开始插入数据,当前环境 = ", env)
+    file_path = file_dir_config(env)
+    zip_files = zip_file_list(env)
     for i in zip_files:
-        traverse_to_path = 'C:\\Win_sharefolder_summary\\' + os.path.splitext(i)[0]
+        traverse_to_path = file_path + os.path.splitext(i)[0]
         # 遍历目录
         insert_file_list = traverse_directory(traverse_to_path)
         # 获取表名
         tabes_name = 'samp_' + os.path.splitext(i)[0][-4:]
-        #开始插入
-        insert_mysql("test", insert_file_list, tabes_name)
+        # 开始插入(表明忽略大小写)
+        insert_mysql(env, insert_file_list, tabes_name.lower())
 
     #清理
     clear_directories(env)
